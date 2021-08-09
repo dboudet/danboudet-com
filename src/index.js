37e632b1af8a7063f8c7212c4b5da97943cda736
Dan Boudet <dboudet04@gmail.com>
2021-08-09 13:10:40 -0400

    testing smoothscroll-polyfill

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import smoothscroll from 'smoothscroll-polyfill';
+smoothscroll.polyfill()
 
 ReactDOM.render(
   <React.StrictMode>
